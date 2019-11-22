887e38bc7b4ab56affdd6b182b5efdf640dee629
samuel <samuelgarciacompanys@gmail.com>
2019-11-22 13:29:34 +0100

    added randomized tables

--- src/main_game.js
+++ src/main_game.js
@@ -1,10 +1,41 @@
 import gameBoard from './factories/gameBoard';
 import newShip from './factories/ship';
+import player from './factories/player';
 import DomTasks from './DomTasks/dom_manager';
 
 
 const gameController = (() => {
+  const initializeBoard2 = () => {
+
+    const board = gameBoard();
+    const ship1 = newShip(1);
+    const ship2 = newShip(2);
+    const ship3 = newShip(3);
+    const ship4 = newShip(4);
+    const ship5 = newShip(5);
+
+    let r1 = Math.round(Math.random()*9)
+    board.placeShip(ship1, [[r1, 9]]);
+
+    let r2 = Math.round(Math.random()*7)
+    board.placeShip(ship2, [[8, r2], [8, r2+1]]);
+
+    let r3 =  Math.round(Math.random()*2)
+    let r3s = Math.round(Math.random()*8)
+    board.placeShip(ship3, [[r3+3, r3s], [r3+4, r3s], [r3+5, r3s]]);
+
+    let r4 =  Math.round(Math.random()*5)
+    board.placeShip(ship4, [[9, r4], [9, r4+1], [9, r4+2], [9, r4+3]]);
+
+    let r5 = Math.round(Math.random()*4)
+    let r5s = Math.round(Math.random()*2)
+    board.placeShip(ship5, [[r5s, r5], [r5s, r5+1], [r5s, r5+2], [r5s, r5+3], [r5s, r5+4]]);
+    return board;
+
+  };
+
   const initializeBoard1 = () => {
+
     const board = gameBoard();
     const ship1 = newShip(1);
     const ship2 = newShip(2);
@@ -12,26 +43,21 @@ const gameController = (() => {
     const ship4 = newShip(4);
     const ship5 = newShip(5);
     
-    board.placeShip(ship1, [[0, 0]]);
-    board.placeShip(ship2, [[2, 0], [2, 1]]);
-    board.placeShip(ship3, [[5, 5], [6, 5], [7, 5]]);
-    board.placeShip(ship4, [[9, 2], [9, 3], [9, 4], [9, 5]]);
-    board.placeShip(ship5, [[8, 4], [8, 5], [8, 6], [8, 7], [8, 8]]);
-    return board;
-  };
+    board.placeShip(ship1, [[0, Math.round(Math.random()*9)]]);
 
-  const initializeBoard2 = () => {
-    const board = gameBoard();
-    const ship1 = newShip(1);
-    const ship2 = newShip(2);
-    const ship3 = newShip(3);
-    const ship4 = newShip(4);
-    const ship5 = newShip(5);
-    board.placeShip(ship1, [[9, 9]]);
-    board.placeShip(ship2, [[8, 0], [8, 1]]);
-    board.placeShip(ship3, [[5, 4], [6, 4], [7, 4]]);
-    board.placeShip(ship4, [[9, 2], [9, 3], [9, 4], [9, 5]]);
-    board.placeShip(ship5, [[2, 4], [2, 5], [2, 6], [2, 7], [2, 8]]);
+    let r2 = Math.round(Math.random()*3)
+    let r2s = Math.round(Math.random()*6)
+    board.placeShip(ship2, [[r2s+1, r2], [r2s+1, r2+1]]);
+
+    let r3 = Math.round(Math.random()*4)
+    let r3s = Math.round(Math.random()*4)
+    board.placeShip(ship3, [[r3+1, r3s+5], [r3+2, r3s+5], [r3+3, r3s+5]]);
+
+    let r4= Math.round(Math.random()*6)
+    board.placeShip(ship4, [[9, r4], [9, r4+1], [9, r4+2], [9, r4+3]]);
+
+    let r5 = Math.round(Math.random()*5)
+    board.placeShip(ship5, [[8, r5], [8, r5+1], [8, r5+2], [8, r5+3], [8, r5+4]]);
     return board;
   };
 
