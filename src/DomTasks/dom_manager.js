80d14e93244d06bfbd7a17af3941c1c5b44221fa
wathigo <wathigosimon@gmail.com>
2019-11-22 10:48:35 -0500

    Fix a bug in chosing the random ship

--- src/DomTasks/dom_manager.js
+++ src/DomTasks/dom_manager.js
@@ -131,6 +131,7 @@ const DomTasks = (() => {
   const renderBoards = () => {
     let boardsAvailable = [gameController.initializeBoard1(), gameController.initializeBoard2(),  gameController.initializeBoard3()]
     const playerBoard = boardsAvailable[Math.round(Math.random()*2)];
+    boardsAvailable.splice(boardsAvailable.indexOf(playerBoard), 1);
     const aiBoard = boardsAvailable[Math.round(Math.random()*2)];
     const playerai = player();
     drawBoards();
