077571a34b85b5b39c1fca6c2244f10a0e9b94f4
samuel <samuelgarciacompanys@gmail.com>
2019-11-22 17:19:16 +0100

    increased complexity of the game for the user

--- src/main_game.js
+++ src/main_game.js
@@ -23,11 +23,11 @@ const gameController = (() => {
     let r3 =  Math.round(Math.random()*2)
     let r3s = Math.round(Math.random()*8)
     board.placeShip(ship3, [[r3+3, r3s], [r3+4, r3s], [r3+5, r3s]]);
+    if(ai === false){
 
     let r4 =  Math.round(Math.random()*5)
     board.placeShip(ship4, [[9, r4], [9, r4+1], [9, r4+2], [9, r4+3]]);
 
-    if(ai === false){
       let r5 = Math.round(Math.random()*4)
       let r5s = Math.round(Math.random()*2)
       board.placeShip(ship5, [[r5s, r5], [r5s, r5+1], [r5s, r5+2], [r5s, r5+3], [r5s, r5+4]]);
@@ -54,12 +54,12 @@ const gameController = (() => {
 
     let r3s = Math.round(Math.random()*4)
     board.placeShip(ship3, [[7, r3s], [7, r3s+1], [7, r3s+2]]);
+    if(ai===false){
 
     let r4= Math.round(Math.random()*3)
     let r4s= Math.round(Math.random()*1)
     board.placeShip(ship4, [[r4s+8, r4], [r4s+8, r4+1], [r4s+8, r4+2], [r4s+8, r4+3]]);
 
-    if(ai===false){
       let r5 = Math.round(Math.random()*5)
       let r5s = Math.round(Math.random()*2)
       board.placeShip(ship5, [[r5,r5s+7], [r5+1, r5s+7], [r5+2, r5s+7], [r5+3, r5s+7], [r5+4, r5s+7]]);
@@ -86,11 +86,11 @@ const gameController = (() => {
     let r3 = Math.round(Math.random()*4)
     let r3s = Math.round(Math.random()*4)
     board.placeShip(ship3, [[r3+1, r3s+5], [r3+2, r3s+5], [r3+3, r3s+5]]);
+    if(ai === false){
 
     let r4= Math.round(Math.random()*6)
     board.placeShip(ship4, [[9, r4], [9, r4+1], [9, r4+2], [9, r4+3]]);
 
-    if(ai === false){
       let r5 = Math.round(Math.random()*5)
       board.placeShip(ship5, [[8, r5], [8, r5+1], [8, r5+2], [8, r5+3], [8, r5+4]]);
     }
